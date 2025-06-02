a0a4966cbaaab13022d1329bafbd75bcd4b1d192
Livo0 <luciferking5999@gmail.com>
2025-06-02 10:29:48 +0000

    layout title

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Game Champions Dashboard',
+  title: 'Dragons Lair',
   description: 'Manage game events, participants, and points',
 };
 
